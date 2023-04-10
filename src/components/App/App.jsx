d4e4963293f789a2cec72aeca7ae18787786541c
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-04-10 10:11:16 -0400

    added a i love it button to each photo and got pictures to display on dom

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+  let [imageGallery, setImageGallery] = useState([]);
+  function fetchGallery() {
+    console.log("in fetchGallery");
+    axios
+      .get("/gallery")
+      .then((response) => {
+        setImageGallery(response.data);
+        console.log(imageGallery);
+      })
+      .catch((error) => {
+        console.log(`Error in GET ${error}`);
+        alert("Something Went Wrong");
+      });
+  }
+
+  useEffect(() => {
+    fetchGallery();
+  }, []);
+  // toggle button or an add button?
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <div>
+        {imageGallery.map((item) => (
+          <div key={item.id}>
+            <img src={item.path} alt={item.description} />
+            <h2>{item.description}</h2>
+            <br />
+            <button>I Love It!</button>
+          </div>
+        ))}
       </div>
-    );
+    </div>
+  );
 }
 
 export default App;
